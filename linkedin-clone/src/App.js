f25ff96f2a1549ac4022bafeb585854fe0cb0382
NoemiP94 <m_emi94@hotmail.it>
2023-11-13 15:41:05 +0100

    finish footer

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Aside from './components/Aside'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Aside />
+      <Footer />
     </div>
   )
 }
--- linkedin-clone/src/components/People.jsx
+++ linkedin-clone/src/components/People.jsx
@@ -1,68 +1,76 @@
-import { Button, Col } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import { BsPersonAdd, BsPersonCircle } from 'react-icons/bs'
 
 const People = () => {
   return (
     <>
       <Col className="border rounded-3 p-3 m-2">
-        <div>
-          <h5>Persone che potresti conoscere</h5>
-          <p className="text-secondary">Dalla tua scuola o università</p>
-
-          <div className="d-flex">
-            <div>
-              <BsPersonCircle className="fs-1 me-2 mt-2" />
+        <Row className="flex-column">
+          <Col>
+            <h5>Persone che potresti conoscere</h5>
+            <p className="text-secondary">Dalla tua scuola o università</p>
+          </Col>
+          <Col>
+            <div className="d-flex">
+              <div>
+                <BsPersonCircle className="fs-1 me-2 mt-2" />
+              </div>
+              <div className="lh-1">
+                <p className="fw-bold">Gustavo Oliveira</p>
+                <p>Full-Stack Web Developer</p>
+                <Button
+                  variant="light"
+                  className="text-secondary bg-white border border-black rounded-pill px-3"
+                >
+                  <BsPersonAdd className="me-2" />
+                  Collegati
+                </Button>
+              </div>
             </div>
-            <div className="lh-1">
-              <p className="fw-bold">Gustavo Oliveira</p>
-              <p>Full-Stack Web Developer</p>
-              <Button
-                variant="light"
-                className="text-secondary bg-white border border-black rounded-pill px-3"
-              >
-                <BsPersonAdd className="me-2" />
-                Collegati
-              </Button>
+            <hr />
+            <div className="d-flex">
+              <div>
+                <BsPersonCircle className="fs-1 me-2 mt-2" />
+              </div>
+              <div className="lh-1">
+                <p className="fw-bold">Laura Gaudencio Rose</p>
+                <p>
+                  {' '}
+                  Junior Full stack Developer | Java | SpringBoot | Angular
+                </p>
+                <Button
+                  variant="light"
+                  className="text-secondary bg-white border border-black rounded-pill px-3"
+                >
+                  <BsPersonAdd className="me-2" />
+                  Collegati
+                </Button>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="d-flex">
-            <div>
-              <BsPersonCircle className="fs-1 me-2 mt-2" />
+            <hr />
+            <div className="d-flex">
+              <div>
+                <BsPersonCircle className="fs-1 me-2 mt-2" />
+              </div>
+              <div className="lh-1">
+                <p className="fw-bold">Adiener Lopez</p>
+                <p> Junior Full-Stack Developer</p>
+                <Button
+                  variant="light"
+                  className="text-secondary bg-white border border-black rounded-pill px-3"
+                >
+                  <BsPersonAdd className="me-2" />
+                  Collegati
+                </Button>
+              </div>
             </div>
-            <div className="lh-1">
-              <p className="fw-bold">Laura Gaudencio Rose</p>
-              <p> Junior Full stack Developer | Java | SpringBoot | Angular</p>
-              <Button
-                variant="light"
-                className="text-secondary bg-white border border-black rounded-pill px-3"
-              >
-                <BsPersonAdd className="me-2" />
-                Collegati
-              </Button>
-            </div>
-          </div>
-          <hr />
-          <div className="d-flex">
-            <div>
-              <BsPersonCircle className="fs-1 me-2 mt-2" />
-            </div>
-            <div className="lh-1">
-              <p className="fw-bold">Adiener Lopez</p>
-              <p> Junior Full-Stack Developer</p>
-              <Button
-                variant="light"
-                className="text-secondary bg-white border border-black rounded-pill px-3"
-              >
-                <BsPersonAdd className="me-2" />
-                Collegati
-              </Button>
-            </div>
-          </div>
-          <h5 className="pt-2  text-center border-top text-secondary mt-4">
-            Mostra tutto
-          </h5>
-        </div>
+          </Col>
+          <Col className="border-top mt-2 bottom-part">
+            <h5 className="text-center text-secondary mt-4 pb-3">
+              Mostra tutto
+            </h5>
+          </Col>
+        </Row>
       </Col>
     </>
   )
