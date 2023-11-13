83ba2185d4a9e1df4a5367ef243f58e2de5315a9
Marissa Burca <Marissaburca@gmail.com>
2023-11-13 15:30:35 +0100

    work in progress

--- linkedin-clone/src/components/MainCard.jsx
+++ linkedin-clone/src/components/MainCard.jsx
@@ -1,44 +1,75 @@
 import { Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import {ImPencil} from "react-icons/im"
+import { ImPencil } from "react-icons/im";
 
 const MainCard = () => {
   return (
     <Row>
       <Col className="px-0 elements">
         <Card>
-          <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s" style={{ height: '200px'}} />
+          <Card.Img
+            variant="top"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
+            style={{ height: "200px" }}
+          />
           <Card.Body>
+            <div className="text-end">
+              <ImPencil />
+            </div>
             <Row>
-                <Col className="col-9">
-            <Card.Title>NAME</Card.Title>
-            <Card.Text>
-              PROFESSION
-            </Card.Text>
-            <Card.Text>
-              Where u live
-              <p>Informazioni di contatto</p>
-            </Card.Text>
-            <Card.Text>
-             45 collegamenti
-            </Card.Text>
-            <Button variant="primary">Disponibile per</Button>
-            <Button variant="outline-primary">Aggiungi sezione del profilo</Button>
-            <Button variant="outline-secondary">Altro</Button>
-            <Card.Text>
-                <div>
-                <p>Disponibile a lavorare</p>
-                <p>Ruolo di Front-End Developer</p>
-                <p>Mostra dettagli</p>
-                </div>
-                <div><ImPencil/></div>
-            </Card.Text>
-            </Col>
-            <Col className="col-3">
-                <div><ImPencil/></div>
+              <Col className="col-9">
+                <Card.Title
+                  className="fw-bold mb-0"
+                  style={{ fontSize: "1.2rem" }}
+                >
+                  NAME
+                </Card.Title>
+                <Card.Text className="mb-0" style={{ fontSize: "1rem" }}>
+                  PROFESSION
+                </Card.Text>
+                <Card.Text className="mb-0" style={{ fontSize: "0.9rem" }}>
+                  Where u live ·<span>Informazioni di contatto</span>
+                </Card.Text>
+                <Card.Text>45 collegamenti</Card.Text>{" "}
+              </Col>
+              <Col className="col-3">
+                <div></div>
                 <p>SCHOOL</p>
-            </Col>
+              </Col>
+              <div>
+                <Button
+                  variant="primary"
+                  className="rounded-pill fw-bold me-2"
+                  style={{ width: "150px" }}
+                >
+                  Disponibile per
+                </Button>
+                <Button
+                  variant="outline-primary"
+                  className="rounded-pill fw-bold  me-2"
+                  style={{ width: "250px" }}
+                >
+                  Aggiungi sezione del profilo
+                </Button>
+                <Button
+                  variant="outline-secondary"
+                  className="rounded-pill fw-bold"
+                  style={{ width: "70px" }}
+                >
+                  Altro
+                </Button>
+              </div>
+              <Card.Text className="d-flex" style={{ fontSize: "1rem" }}>
+                <div className="d-flex flex-column">
+                  <span className="mb-0">Disponibile a lavorare</span>
+                  <span>Ruolo di Front-End Developer</span>
+                  <span>Mostra dettagli</span>
+                </div>
+                <div>
+                  <ImPencil />
+                </div>
+              </Card.Text>
             </Row>
           </Card.Body>
         </Card>
--- linkedin-clone/src/components/Visited.jsx
+++ linkedin-clone/src/components/Visited.jsx
@@ -4,13 +4,13 @@ import { BsArrowRight } from "react-icons/bs";
 const Visited = () => {
   return (
     <Row className="elements mb-2">
-      <Col className="d-flex justify-content-between p-3">
-        <div className="d-flex">
-          <div className="mx-2 fw-bold"> 9 isualizzazioni del profilo</div>
-          <div className="text-secondary ">Ultimi 90 gg</div>
+      <Col className="d-flex justify-content-between p-3" style={{fontSize :'0.9rem'}}>
+        <div className="d-flex align-items-end">
+          <div className="fw-bold pe-2"> 9 visualizzazioni del profilo</div>
+          <div className="text-secondary">Ultimi 90 gg</div>
         </div>
         <div className="fw-bold text-secondary ">
-          Visualizza tutte le analisi <BsArrowRight className="" />{" "}
+          Visualizza tutte le analisi <BsArrowRight className="fs-6" />{" "}
         </div>
       </Col>
     </Row>
