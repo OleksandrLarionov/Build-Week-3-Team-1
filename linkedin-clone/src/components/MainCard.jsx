ae8a3851e940c8306cd1dc503efecba861811e1c
Marissa Burca <Marissaburca@gmail.com>
2023-11-13 16:28:28 +0100

    farte del profilo con info pers fatta

--- linkedin-clone/src/components/MainCard.jsx
+++ linkedin-clone/src/components/MainCard.jsx
@@ -7,37 +7,38 @@ const MainCard = () => {
   return (
     <Row>
       <Col className="px-0 elements">
-        <Card>
+        <Card className="position-relative">
           <Card.Img
             variant="top"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
             style={{ height: "200px" }}
           />
-          <Card.Body>
+          <Card.Body className="mt-5">
             <div className="text-end">
-              <ImPencil />
+              <ImPencil className="fs-5" style={{position:'absolute', right:'20px', top:'220px'}}/>
+           <img src="https://platinumlist.net/guide/wp-content/uploads/2023/03/IMG-worlds-of-adventure.webp" alt="profile" style={{height:'150px',width:'150px', borderRadius:'75px', border: '3px solid white', position:'absolute', left:'20px', top:'100px'}}/>
             </div>
             <Row>
               <Col className="col-9">
                 <Card.Title
                   className="fw-bold mb-0"
-                  style={{ fontSize: "1.2rem" }}
+                  style={{ fontSize: "1.4rem" }}
                 >
                   NAME
                 </Card.Title>
-                <Card.Text className="mb-0" style={{ fontSize: "1rem" }}>
+                <Card.Text className="mb-0" style={{ fontSize: "0.9rem" }}>
                   PROFESSION
                 </Card.Text>
-                <Card.Text className="mb-0" style={{ fontSize: "0.9rem" }}>
-                  Where u live ·<span>Informazioni di contatto</span>
+                <Card.Text className="mb-0" style={{ fontSize: "0.8rem" }}>
+                  Where u live ·<span className="text-primary fw-bold">Informazioni di contatto</span>
                 </Card.Text>
-                <Card.Text>45 collegamenti</Card.Text>{" "}
+                <Card.Text className="text-primary fw-bold" style={{ fontSize: "0.8rem" }}>45 collegamenti</Card.Text>{" "}
               </Col>
               <Col className="col-3">
                 <div></div>
                 <p>SCHOOL</p>
               </Col>
-              <div>
+              <div className="mt-3"> 
                 <Button
                   variant="primary"
                   className="rounded-pill fw-bold me-2"
@@ -60,11 +61,11 @@ const MainCard = () => {
                   Altro
                 </Button>
               </div>
-              <Card.Text className="d-flex" style={{ fontSize: "1rem" }}>
-                <div className="d-flex flex-column">
-                  <span className="mb-0">Disponibile a lavorare</span>
+              <Card.Text className="d-flex justify-content-between w-50 mt-3 ms-2 rounded py-2 " style={{ fontSize: "0.9rem",backgroundColor: 'rgb(223,231,240' }}>
+                <div className="d-flex flex-column" style={{ fontSize:"0.8rem" }}>
+                  <span className="mb-0 fw-bold pb-0" >Disponibile a lavorare</span>
                   <span>Ruolo di Front-End Developer</span>
-                  <span>Mostra dettagli</span>
+                  <span className="fw-bold text-primary">Mostra dettagli</span>
                 </div>
                 <div>
                   <ImPencil />
--- linkedin-clone/src/components/MyHome.jsx
+++ linkedin-clone/src/components/MyHome.jsx
@@ -4,7 +4,7 @@ import MainCard from './MainCard';
 
 const MyHome = () => {
     return ( 
-        <Col className="col-xs-12 col-md-8 col-lg-6 col-xl-7 justify-content-center">
+        <Col className="col-xs-12 col-md-8 col-lg-7 justify-content-center">
          <Visited/>
          <MainCard/>
        {/*   <Suggested/>
