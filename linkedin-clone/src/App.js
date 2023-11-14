b99b39f8c3b66cec3435a1bad44339cd2713ac69
NoemiP94 <m_emi94@hotmail.it>
2023-11-14 10:20:55 +0100

    Home

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import Aside from './components/Aside';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
-import TestComp from './components/TestComp';
-import ModalProfiles from './components/modal/ModarlProfiles';
-import { useSelector } from 'react-redux';
+import './App.css'
+import Aside from './components/Aside'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer'
+import TestComp from './components/TestComp'
+import ModalProfiles from './components/modal/ModarlProfiles'
+import { useSelector } from 'react-redux'
+import Home from './components/Home'
 function App() {
-	const modal = useSelector((state) => state.profile.modal && state.profile.modal[0]);
-	return (
-		<>
-			<div className={modal ? 'active-modal' : ''}>
-				<Aside />
+  const modal = useSelector(
+    (state) => state.profile.modal && state.profile.modal[0]
+  )
+  return (
+    <>
+      <div className={modal ? 'active-modal' : ''}>
+        {/* <Aside />
 				<ModalProfiles />
 				<Footer />
-				<TestComp />
-			</div>
-		</>
-	);
+				<TestComp /> */}
+        <Home />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ linkedin-clone/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Home
