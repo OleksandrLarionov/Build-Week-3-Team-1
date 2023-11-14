3ff07f463352ddc0f8dfb774ea22bd240f2ea9b4
Marissa Burca <Marissaburca@gmail.com>
2023-11-14 11:02:15 +0100

    Merge branch 'oleks_redux' into marissa-

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,29 +1,33 @@
-import './App.css';
-import Aside from './components/Aside';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
-import TestComp from './components/TestComp';
-import ModalProfiles from './components/modal/ModarlProfiles';
-import { useSelector } from 'react-redux';
-import MyCentral from './components/MyCentral';
-import { Row} from 'react-bootstrap';
+import "./App.css";
+import Aside from "./components/Aside";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import TestComp from "./components/TestComp";
+import ModalProfiles from "./components/modal/ModarlProfiles";
+import { useSelector } from "react-redux";
+import MyCentral from "./components/MyCentral";
+import { Row } from "react-bootstrap";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
-	const modal = useSelector((state) => state.profile.modal && state.profile.modal[0]);
-	return (
-		<>
-			<div className={modal ? 'active-modal' : ''}>
-
-			<Row className='justify-content-center'>
-    <MyCentral/>
-    </Row>
-				<Aside />
-				<ModalProfiles />
-				<Footer />
-				<TestComp />
-			</div>
-		</>
-	);
+  const modal = useSelector(
+    (state) => state.profile.modal && state.profile.modal[0]
+  );
+  return (
+    <>
+      <div className={modal ? "active-modal" : ""}>
+		<MyNavbar/>
+        <Row className="justify-content-center">
+          <MyCentral /> 
+		  <Aside />
+        </Row>
+       <Footer />
+        <ModalProfiles />
+        
+        <TestComp />
+      </div>
+    </>
+  );
 }
 
 export default App;
