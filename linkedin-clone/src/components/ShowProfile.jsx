75da1a796c96721646741e0e96033f769ce81da4
NoemiP94 <m_emi94@hotmail.it>
2023-11-14 15:24:00 +0100

    home ok

--- linkedin-clone/src/components/ShowProfile.jsx
+++ linkedin-clone/src/components/ShowProfile.jsx
@@ -1,70 +1,21 @@
-import { Card, Col, Row } from 'react-bootstrap'
-
-import { FcPackage } from 'react-icons/fc'
-import { BsBookmarkFill } from 'react-icons/bs'
+import { Col, Row } from 'react-bootstrap'
+import User from './User'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserDataAction } from '../redux/action'
 
 const ShowProfile = () => {
+  const mainUser = useSelector(
+    (state) => state.user.userData && state.user.userData[0]
+  )
+
+  const dispatch = useDispatch()
+  dispatch(getUserDataAction())
+
+  console.log(mainUser)
   return (
     <Row>
       <Col className="px-0 mb-1 border rounded-3 m-3 w-100">
-        <Card
-          className="position-relative border-0 text-size"
-          style={{ border: '1px solid rgb(223,222,219)' }}
-        >
-          <Card.Img
-            variant="top"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
-            style={{ height: '60px' }}
-          />
-          <Card.Body className="mt-5 text-center pb-0">
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
-                alt="profile"
-                style={{
-                  height: '75px',
-                  width: '75px',
-                  borderRadius: '75px',
-                  border: '3px solid white',
-                  position: 'absolute',
-                  left: '115px',
-                  top: '30px',
-                }}
-              />
-            </div>
-            <Row>
-              <Col className="lh-sm">
-                <Card.Title className="fw-bold mb-0">name</Card.Title>
-                <Card.Text className="mb-3">profession</Card.Text>
-              </Col>
-              <hr className="m-0" />
-              <Col className="lh-sm bottom-part pt-3">
-                <div className="d-flex justify-content-between">
-                  <p className="text-secondary mb-0">Collegamento</p>
-                  <p className="text-primary mb-0">1</p>
-                </div>
-                <p className="text-start fw-bold">Espandi la tua rete</p>
-              </Col>
-              <hr className="m-0" />
-              <Col className="bottom-part pt-3">
-                <p className="text-secondary mb-0 text-start lh-1">
-                  Accedi a strumenti e informazioni in esclusiva
-                </p>
-                <div className="d-flex justify-content-start">
-                  <FcPackage className="mt-1" />
-                  <p className=" ms-1 text-decoration-underline fw-bold">
-                    Prova Premium per 0 EUR
-                  </p>
-                </div>
-              </Col>
-              <hr className="m-0" />
-              <Col className="bottom-part pt-3 pb-2 d-flex align-items-center justify-content-start">
-                <BsBookmarkFill className="text-secondary me-2 mb-2" />
-                <h6>I miei elementi</h6>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
+        {mainUser && <User user={mainUser} />}
       </Col>
     </Row>
   )
