7521e470d9f046363b8f47f7b502d480da4ceb96
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-11-14 10:11:08 +0100

    Update

--- linkedin-clone/src/components/MyNavbar.jsx
+++ linkedin-clone/src/components/MyNavbar.jsx
@@ -28,7 +28,16 @@ const MyNavbar = () => {
       <Row className="d-flex me-4  mb-3">
         <Col md={8} className="d-flex align-items-center">
           <div className="d-flex align-items-center ">
-            <Navbar.Brand href="#">
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <div className="d-flex justify-content-center align-items-center">
+
+                <Navbar.Brand href="#">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -39,29 +48,27 @@ const MyNavbar = () => {
                 height="40"
                 focusable="false"
                 style={{ color: "#0A66C2" }}
+               
+                
               >
                 <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
               </svg>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <div className="d-flex justify-content-center align-items-center">
-                  <Form>
+
+                  <Form style={{width:"24rem"}} className="m me-4">
                     <Form.Control
                       type="search"
                       placeholder="Search"
-                      className="me-2"
+                      className="me-4 ms-1 ps-3"
                       aria-label="Search"
                       id="color-placeholder"
+                      
+                      
                     />
                   </Form>
                 </div>
-
+                {/* 2 Div principale Navbar */}
+             <div className="d-flex justify-content-around align-items-center">
                 <div className="d-flex flex-column ">
                   <Nav.Link href="#">
                     <p className="text-center">
@@ -106,8 +113,10 @@ const MyNavbar = () => {
                     </p>
                   </Nav.Link>
                 </div>
+            </div> 
+
                 <Nav.Link href="#">
-                  <div className="d-flex flex-column align-items-center">
+                  <div className="d-flex flex-column align-items-center me-4 ">
                     <img
                       src="https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                       alt="cane"
@@ -173,16 +182,17 @@ const MyNavbar = () => {
             </Navbar.Collapse>
           </div>
         </Col>
+        
         <Col md={4} className="d-flex align-items-center">
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex flex-column  align-items-center ">
+          <div className="d-flex justify-content-between align-items-center ms-4 ">
+            <div className="d-flex flex-column align-items-center mt-1 ">
               <BsFillGrid3X3GapFill className="fs-4 text-secondary " />
               <NavDropdown
                 title="Per le aziende"
                 id="navbarScrollingDropdown"
                 className="mb-2"
               >
-                <h5 className="ms-1 mb-4">Per le aziende</h5>
+                <h5 className="ms-1 mt-0">Per le aziende</h5>
                 <h6 className="text-start ms-3">
                   Scopri altri prodotti LinkedIn
                 </h6>
