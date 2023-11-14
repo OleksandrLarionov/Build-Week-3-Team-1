01815e6010447309eec41ba60fc789791a6d9bbe
Marissa Burca <Marissaburca@gmail.com>
2023-11-14 12:58:28 +0100

    12

--- linkedin-clone/src/components/Aside.jsx
+++ linkedin-clone/src/components/Aside.jsx
@@ -7,7 +7,7 @@ import Advertising from "./AsideComponents/Advertising";
 const Aside = () => {
   return (
     <>
-      <Col sm={10} md={4} lg={3} xl={2}>
+      <Col sm={12} md={12} lg={3} xl={2}>
         <Setup />
         <People />
         <InLearning />
