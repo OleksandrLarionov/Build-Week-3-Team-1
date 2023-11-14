5c3da0a4a618d0354aca81115c61d3f1dfa7fdca
Marissa Burca <Marissaburca@gmail.com>
2023-11-14 23:02:41 +0100

    piccoli dettagli

--- linkedin-clone/src/components/MyNavbar.jsx
+++ linkedin-clone/src/components/MyNavbar.jsx
@@ -1,19 +1,31 @@
-import {Col,Row, Offcanvas, Button, NavLink, Form, Nav, Navbar,NavDropdown} from 'react-bootstrap'
+import {
+  Col,
+  Row,
+  Offcanvas,
+  Button,
+  NavLink,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 import { HiMiniUsers } from "react-icons/hi2";
-import { PiHandbagSimpleFill } from "react-icons/pi";
-import { BsFillGrid3X3GapFill, BsFillBellFill ,BsPlus } from "react-icons/bs";
-import { MdOutlineMenuBook,MdArrowDropDown } from "react-icons/md";
-import { TbFileAnalytics } from "react-icons/tb";
-import { FaCompass, FaUsers } from "react-icons/fa";
-import { ImUserCheck } from "react-icons/im";
-import { FcAdvertising, FcTemplate } from "react-icons/fc";
-import { AiFillHome, AiFillMessage } from 'react-icons/ai'
+import { PiHandbagSimpleFill, PiVideoFill } from "react-icons/pi";
+import { BsFillGrid3X3GapFill, BsFillBellFill, BsPlus } from "react-icons/bs";
+import { MdArrowDropDown } from "react-icons/md";
+import { FaCompass } from "react-icons/fa";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import {
+  FcAdvertising,
+  FcTemplate,
+  FcCollaboration,
+  FcBarChart,
+} from "react-icons/fc";
+import { AiFillHome, AiFillMessage } from "react-icons/ai";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { handleSubmitAction } from "../redux/action";
-
-
 
 const MyNavbar = () => {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
@@ -259,100 +271,214 @@ const MyNavbar = () => {
             </Col>
 
             <Col className="col-3 d-flex align-items-center position-relative">
-      <div className="d-flex justify-content-between align-items-center ">
-        <div className="d-flex justify-content-end mt-3">
-          <div className="text-center" onClick={toggleOffcanvas}>
-            <BsFillGrid3X3GapFill className="fs-4 text-secondary" />
-            <p>Per le aziende<MdArrowDropDown /></p>
-          </div>
-        </div>
-        <Offcanvas placement="end" show={showOffcanvas} onHide={() => setShowOffcanvas(false)}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title className='fw-bold'>Per le aziende</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-			<Row className="d-flex border rounded mx-2 ">
-				<Col className=" col-12 d-flex pt-2 justify-content-between">
-				<div className="text-center" style={{width:'100px'}}>
-                  <MdOutlineMenuBook className="text-info fs-1 border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Learning</p>
+              <div className="d-flex justify-content-between align-items-center ">
+                <div className="d-flex justify-content-end mt-3">
+                  <div className="text-center" onClick={toggleOffcanvas}>
+                    <BsFillGrid3X3GapFill className="fs-4 text-secondary" />
+                    <p>
+                      Per le aziende
+                      <MdArrowDropDown />
+                    </p>
+                  </div>
                 </div>
-				<div className="text-center" style={{width:'100px'}}>
-                  <TbFileAnalytics className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Talent Insights</p>
-                </div>
-				<div className="text-center" style={{width:'100px'}}>
-                  <FcTemplate className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Pubblica un’offerta di lavoro</p>
-                </div>
-				<div className="text-center" style={{width:'100px'}}>
-                  <FcAdvertising className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Pubblicizza</p>
-                </div>
-				</Col>
-				<Col className=" col-12 d-flex justify-content-start" >
-				<div className="text-center" style={{width:'85px'}}>
-                  <FaCompass  className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Trova nuovi clienti</p>
-                </div>
-				<div className="text-center" style={{width:'70px'}}>
-                  <FaUsers className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Gruppi</p>
-                </div>
-				<div className="text-center" style={{width:'95px'}}>
-                  <ImUserCheck className="text-info fs-1  border " />
-					<p className="lh-1 pt-2" style={{fontSize:'0.9rem'}}>Marketplace dei servizi</p>
-                </div>
-				</Col>
-			</Row>
-			<Row className="border rounded-top mx-2 mt-2 ps-4" >
-			<h6 className="text-start mt-2 fw-bold ">
-                        Scopri altro per il business
-                      </h6>
-			<div className="fw-bold">
-               <div >
-				<p  className="lh-1 mb-1" style={{fontSize:'0.9rem'}}> Assumi su LinkedIn</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}>Trova, attrai e assumi</p>
-			   </div>
-               <div>
-				<p className="lh-1 mb-1" style={{fontSize:'0.9rem'}}>Vendi con LinkedIn</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}>Costruisci relazioni con i buyer</p>
-			   </div>
-               <div>
-				<p className="lh-1 mb-1" style={{fontSize:'0.9rem'}}>Offerta di lavoro gratuita</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}> Trova candidati di qualità</p>
-			   </div>
-               <div>
-				<p className="lh-1 mb-1" style={{fontSize:'0.9rem'}}>Fai pubblicità su LinkedIn</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}>Acquisisci clienti e fai crescere la tua azienda</p>
-			   </div>
-               <div>
-				<p className="lh-1 mb-1" style={{fontSize:'0.9rem'}}>Impara con LinkedIn</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}> Corsi per formare i tuoi dipendenti</p>
-			   </div>
-               <div>
-				<p className="lh-1 mb-1" style={{fontSize:'0.9rem'}}>Centro amministrazione</p>
-				<p className="fw-normal lh-1" style={{fontSize:'0.8rem'}}>Gestisci i dettagli di fatturazione e account</p>
-			   </div>
-			</div>
+                <Offcanvas
+                  placement="end"
+                  show={showOffcanvas}
+                  onHide={() => setShowOffcanvas(false)}
+                >
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title className="fw-bold">
+                      Per le aziende
+                    </Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    <Row className="d-flex border rounded mx-2 ">
+                      <Col className=" col-12 d-flex pt-2 justify-content-between">
+                        <div className="text-center" style={{ width: "100px" }}>
+                          <PiVideoFill className="text-primary fs-1 border" />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Learning
+                          </p>
+                        </div>
+                        <div className="text-center" style={{ width: "100px" }}>
+                          <FcBarChart className="fs-1  border  " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Talent Insights
+                          </p>
+                        </div>
+                        <div className="text-center" style={{ width: "100px" }}>
+                          <FcTemplate className="text-info fs-1  border " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Pubblica un’offerta di lavoro
+                          </p>
+                        </div>
+                        <div className="text-center" style={{ width: "100px" }}>
+                          <FcAdvertising className="text-info fs-1  border " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Pubblicizza
+                          </p>
+                        </div>
+                      </Col>
+                      <Col className=" col-12 d-flex justify-content-start">
+                        <div className="text-center" style={{ width: "85px" }}>
+                          <FaCompass className="text-info fs-1  border " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Trova nuovi clienti
+                          </p>
+                        </div>
+                        <div className="text-center" style={{ width: "70px" }}>
+                          <FcCollaboration className="text-info fs-1  border " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Gruppi
+                          </p>
+                        </div>
+                        <div className="text-center" style={{ width: "95px" }}>
+                          <FaMoneyBillTrendUp className="text-info fs-1  border " />
+                          <p
+                            className="lh-1 pt-2"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Marketplace dei servizi
+                          </p>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row className="border rounded-top mx-2 mt-2">
+                      <div className="border-bottom ps-0">
+                        <h6 className="text-start mt-2 fw-bold ps-4">
+                          Scopri altro per il business
+                        </h6>
+                      </div>
+					  <div className=" ps-4 pt-2">
+                      <div className="fw-bold">
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            {" "}
+                            Assumi su LinkedIn
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            Trova, attrai e assumi
+                          </p>
+                        </div>
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Vendi con LinkedIn
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            Costruisci relazioni con i buyer
+                          </p>
+                        </div>
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Offerta di lavoro gratuita
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            {" "}
+                            Trova candidati di qualità
+                          </p>
+                        </div>
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Fai pubblicità su LinkedIn
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            Acquisisci clienti e fai crescere la tua azienda
+                          </p>
+                        </div>
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Impara con LinkedIn
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            {" "}
+                            Corsi per formare i tuoi dipendenti
+                          </p>
+                        </div>
+                        <div>
+                          <p
+                            className="lh-1 mb-1"
+                            style={{ fontSize: "0.9rem" }}
+                          >
+                            Centro amministrazione
+                          </p>
+                          <p
+                            className="fw-normal lh-1"
+                            style={{ fontSize: "0.8rem" }}
+                          >
+                            Gestisci i dettagli di fatturazione e account
+                          </p>
+                        </div>
+						</div>
+                      </div>
+                    </Row>
+                    <Row className="border border-top-0 rounded-bottom mx-2 ps-4">
+                      <p
+                        className="lh-1 mb-1 fw-bold py-2"
+                        style={{ fontSize: "0.9rem" }}
+                      >
+                        Crea una pagina aziendale <BsPlus className="fs-4" />
+                      </p>
+                    </Row>
+                  </Offcanvas.Body>
+                </Offcanvas>
+              </div>
 
-			</Row>
-			<Row className="border border-top-0 rounded-bottom mx-2 ps-4" >
-			<p className="lh-1 mb-1 fw-bold py-2" style={{fontSize:'0.9rem'}}>Crea una pagina aziendale <BsPlus className="fs-4"/></p>
-			</Row >
-			</Offcanvas.Body>
-        </Offcanvas>
-      </div>
-   
-                <div className="">
-                  <NavLink
-                    className="fw-medium text-center px-1 different "
-                    style={{ width: "100px" }}
-                  >
-                    Prova Premium per 0 EUR
-                  </NavLink>
-                </div>
-              
+              <div className="">
+                <NavLink
+                  className="fw-medium text-center px-1 different "
+                  style={{ width: "100px" }}
+                >
+                  Prova Premium per 0 EUR
+                </NavLink>
+              </div>
             </Col>
           </Row>
         </Navbar>
