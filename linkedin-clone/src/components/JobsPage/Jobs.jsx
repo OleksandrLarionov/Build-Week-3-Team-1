0ede00ed28d492c46c8049f8ff282243015fb341
NoemiP94 <m_emi94@hotmail.it>
2023-11-15 13:03:06 +0100

    work in progress on job component

--- linkedin-clone/src/components/JobsPage/Jobs.jsx
+++ linkedin-clone/src/components/JobsPage/Jobs.jsx
@@ -1,26 +1,35 @@
-import { Col, Row } from "react-bootstrap"
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { Col, Container, Row } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const Jobs=()=>{
-  const jobsData=useSelector((state)=>state.job.list) 
-    return( jobsData.map((job)=>{
-     return( <Row
-        className="mx-0 mt-3 p-3"
-        style={{ border: '1px solid #00000033', borderRadius: 4 }}
-      >
-        <Col xs={3}>
-          <Link to={`${job.url}`} >{job.company_name} </Link>
-        </Col>
-        <Col xs={9} className='d-flex justify-content-between'>
-          <a href={job.url} target="_blank" rel="noreferrer">
-            {job.title}
-          </a>        
-        </Col>
-      </Row>)
-    })
-        
-    )
-    }
-    
-    export default Jobs
+const Jobs = () => {
+  const jobsData = useSelector((state) => state.job.list)
+  return (
+    <Container>
+      <Row className="mx-0 mt-3 p-3 justify-content-center">
+        {jobsData.map((job) => {
+          return (
+            <div
+              className="w-25 elements m-2 p-2 text-center rounded-3 d-flex flex-column justify-content-between "
+              style={{ border: '1px solid #00000033', borderRadius: 4 }}
+            >
+              <Col>
+                <Link to={`${job.url}`}>
+                  Azienda <br />
+                  {job.company_name}{' '}
+                </Link>
+              </Col>
+              <Col>
+                <a href={job.url} target="_blank" rel="noreferrer">
+                  {job.title}
+                </a>
+              </Col>
+            </div>
+          )
+        })}
+      </Row>
+    </Container>
+  )
+}
+
+export default Jobs
