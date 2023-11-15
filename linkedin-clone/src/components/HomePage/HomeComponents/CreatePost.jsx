9e2eae98a7faf31c70096384f4ac93b0e1c48523
NoemiP94 <m_emi94@hotmail.it>
2023-11-15 12:03:32 +0100

    correzione grandezze colonne home

--- linkedin-clone/src/components/HomePage/HomeComponents/CreatePost.jsx
+++ linkedin-clone/src/components/HomePage/HomeComponents/CreatePost.jsx
@@ -3,7 +3,7 @@ import { FcCalendar, FcPicture, FcTemplate } from 'react-icons/fc'
 
 const CreatePost = () => {
   return (
-    <Row className="px-0 mb-1 border rounded-3 m-2 flex-column py-3 elements">
+    <Row className="w-100 px-0 mb-1 border rounded-3 mt-0 mx-0 flex-column py-3 elements">
       <Col className="d-flex align-items-center">
         <img
           src="https://platinumlist.net/guide/wp-content/uploads/2023/03/IMG-worlds-of-adventure.webp"
@@ -22,8 +22,8 @@ const CreatePost = () => {
         />
       </Col>
       <Col className="d-flex  w-100 justify-content-center pt-3">
-        <Row className="w-100 flex-md-column text-secondary">
-          <Col className="d-flex justify-content-md-around">
+        <Row className="w-100 text-secondary">
+          <Col className="d-flex justify-content-between">
             <div className="d-flex align-items-center">
               <FcPicture className="mb-3 me-2 fs-4" />
               <p>Contenuti multimediali</p>
@@ -32,10 +32,10 @@ const CreatePost = () => {
               <FcCalendar className="mb-3 me-2 fs-4" />
               <p>Evento</p>
             </div>
-          </Col>
-          <Col className="d-flex align-items-center justify-content-center">
-            <FcTemplate className="mb-3 me-2 fs-4" />
-            <p>Scrivi un articolo</p>
+            <div className="d-flex align-items-center">
+              <FcTemplate className="mb-3 me-2 fs-4" />
+              <p>Scrivi un articolo</p>
+            </div>
           </Col>
         </Row>
       </Col>
--- linkedin-clone/src/components/HomePage/HomeComponents/User.jsx
+++ linkedin-clone/src/components/HomePage/HomeComponents/User.jsx
@@ -5,65 +5,70 @@ import { FcPackage } from 'react-icons/fc'
 const User = ({ user }) => {
   return (
     <>
-      <Card
-        className="position-relative border-0 text-size"
-        style={{ border: '1px solid rgb(223,222,219)' }}
-      >
-        <Card.Img
-          variant="top"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
-          style={{ height: '60px' }}
-        />
-        <Card.Body className="mt-5 text-center pb-0">
-          <div>
+      <Row className="mb-2">
+        <Col>
+          <Card
+            className=" border text-size overflow-hidden"
+            style={{ border: '1px solid rgb(223,222,219)' }}
+          >
             <img
-              src={user.image}
-              alt="profile"
-              style={{
-                height: '75px',
-                width: '75px',
-                borderRadius: '75px',
-                border: '3px solid white',
-                position: 'absolute',
-                left: '115px',
-                top: '30px',
-              }}
-              className="z-1 bg-white"
+              variant="top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s"
+              style={{ height: '60px' }}
+              alt="background"
             />
-          </div>
-          <Row>
-            <Col className="lh-sm">
-              <Card.Title className="fw-bold mb-0">{user.name}</Card.Title>
-              <Card.Text className="mb-3">{user.title}</Card.Text>
-            </Col>
-            <hr className="m-0" />
-            <Col className="lh-sm bottom-part pt-3">
-              <div className="d-flex justify-content-between">
-                <p className="text-secondary mb-0">Collegamento</p>
-                <p className="text-primary mb-0">1</p>
+            <Card.Body className=" mt-5 text-center pb-0">
+              <div className="position-relative d-flex justify-content-center">
+                <img
+                  src={user.image}
+                  alt="profile"
+                  style={{
+                    height: '75px',
+                    width: '75px',
+                    borderRadius: '75px',
+                    border: '3px solid white',
+                    position: 'absolute',
+                    left: '195px',
+                    top: '-75px',
+                  }}
+                  className="z-1 bg-white"
+                />
               </div>
-              <p className="text-start fw-bold">Espandi la tua rete</p>
-            </Col>
-            <hr className="m-0" />
-            <Col className="bottom-part pt-3">
-              <p className="text-secondary mb-0 text-start lh-1">
-                Accedi a strumenti e informazioni in esclusiva
-              </p>
-              <div className="d-flex justify-content-start">
-                <FcPackage className="mt-1" />
-                <p className=" ms-1 text-decoration-underline fw-bold">
-                  Prova Premium per 0 EUR
-                </p>
-              </div>
-            </Col>
-            <hr className="m-0" />
-            <Col className="bottom-part pt-3 pb-2 d-flex align-items-center justify-content-start">
-              <BsBookmarkFill className="text-secondary me-2 mb-2" />
-              <h6>I miei elementi</h6>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+              <Row>
+                <Col className="lh-sm">
+                  <Card.Title className="fw-bold mb-0">{user.name}</Card.Title>
+                  <Card.Text className="mb-3">{user.title}</Card.Text>
+                </Col>
+                <hr className="m-0" />
+                <Col className="lh-sm bottom-part pt-3">
+                  <div className="d-flex justify-content-between">
+                    <p className="text-secondary mb-0">Collegamento</p>
+                    <p className="text-primary mb-0">1</p>
+                  </div>
+                  <p className="text-start fw-bold">Espandi la tua rete</p>
+                </Col>
+                <hr className="m-0" />
+                <Col className="bottom-part pt-3">
+                  <p className="text-secondary mb-0 text-start lh-1">
+                    Accedi a strumenti e informazioni in esclusiva
+                  </p>
+                  <div className="d-flex justify-content-start">
+                    <FcPackage className="mt-1" />
+                    <p className=" ms-1 text-decoration-underline fw-bold">
+                      Prova Premium per 0 EUR
+                    </p>
+                  </div>
+                </Col>
+                <hr className="m-0" />
+                <Col className="bottom-part pt-3 pb-2 d-flex align-items-center justify-content-start">
+                  <BsBookmarkFill className="text-secondary me-2 mb-2" />
+                  <h6>I miei elementi</h6>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
